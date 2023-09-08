eb4b6dd9642e5491842a754ab7e00303aa938691
Z-Sar08 <133613281+Z-Sar08@users.noreply.github.com>
2023-09-08 10:09:34 +0800

    Update app.js
    multiply and subtract

--- script/app.js
+++ script/app.js
@@ -1,9 +1,20 @@
-function displaySum() {
+function displayProd() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
-  let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  let total = firstNum * secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
 }
 
-document.getElementById('sumButton').addEventListener("click", displaySum);
+document.getElementById('prodButton').addEventListener("click", displayProd);
+
+
+function displayDiff() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
+}
+
+document.getElementById('diffButton').addEventListener("click", displayDiff);
